8a500c5565cd4f27293950d61a89ee0e84ff1493
Clayton <clayton@claytonlz.com>
2012-12-05 13:30:15 -0700

    Generic config file

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 // e.g. http://ci.example.com/api/json
 
 var config = {
-    ci_json_url:"http://ci.smdev.info/view/Radiator/api/json",
-    refresh_interval: 60000
+	ci_json_url:"http://ci.example.com/api/json",
+	refresh_interval: 60000,
+	filtered: ["Filtered Build"]
 }
